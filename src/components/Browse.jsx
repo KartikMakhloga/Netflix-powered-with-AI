9c39fb3a1111d3da112b2c8dfe1c59554bae955d
Kartik Makhloga <kartikmakhoga20@gmail.com>
2024-07-18 11:54:49 +0530

    fetch movies from OMDB

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
 
 const Browse = () => {
+  const fetchMovies = async () => {
+    const response = await fetch(`https://www.omdbapi.com/?apikey=bfa491dd&s=all`)
+    const data = await response.json()
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }
+  , [])
+
   return (
     <div className='text-white'>
       <Header />
